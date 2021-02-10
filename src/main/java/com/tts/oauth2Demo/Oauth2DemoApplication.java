91536189215f4afc6dfbc8a26c9c9da4c1a84968
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-02-10 08:44:44 -0600

    Configured HttpSecurity

--- src/main/java/com/tts/oauth2Demo/Oauth2DemoApplication.java
+++ src/main/java/com/tts/oauth2Demo/Oauth2DemoApplication.java
@@ -2,12 +2,55 @@ package com.tts.oauth2Demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
-public class Oauth2DemoApplication {
+@RestController
+public class Oauth2DemoApplication extends WebSecurityConfigurerAdapter {
+
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2DemoApplication.class, args);
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests(a -> a
+					.antMatchers("/", "/error").permitAll()
+					.anyRequest().authenticated()
+				)
+				.exceptionHandling(e -> e
+					.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+				)
+				.oauth2Login().defaultSuccessUrl("/", true);
+	}
+
 }
+
+/*
+	Here we make a /user endpoint. This sends back the currently logged-in user.
+
+	WebSecurityConfigurerAdapter configures security filter chain that carries the OAuth 2.0
+	authentication processor. This is so users can view the homepage without being logged in.
+
+	HttpSecurity is configured to make the dynamic page, '/' be able to make some content visible to
+	unauthenticated users.
+	We allowed /error
+	Everything, including /user remains secure unless indicated because of the .anyRequest().authenticated()
+	configuration at the end.
+ */
